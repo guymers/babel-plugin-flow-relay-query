154abb7a6c19562ce9d734cc5a6c749fae764850
Sam Guymer <sam@guymer.me>
2017-01-19 21:03:14 +1000

    Add Relay.createContainer HOC test

---
+++ test/fixture/fragment/relay_createcontainer_hoc/expected.js
@@ -0,0 +1,50 @@
+/* @flow */
+import React from "react";
+import Relay from "react-relay";
+
+
+type ArticleProps = {
+  article: {
+    title: string;
+    posted: string;
+    content: string;
+    views: number;
+    sponsored: boolean;
+
+    author: {
+      name: string;
+      email: string;
+    };
+  }
+};
+
+class Article extends React.Component<void, ArticleProps, void> {
+  render() {
+    const { article } = this.props;
+    return <div>
+        <div>{article.title} ({article.posted})</div>
+        <div>{article.author.name} [{article.author.email}]</div>
+        <div>{article.content}</div>
+      </div>;
+  }
+}
+
+const RelayHOC = (component, options) => Relay.createContainer(component, options);
+
+export default RelayHOC(Article, {
+  fragments: {
+    article: () => Relay.QL`
+fragment on Article {
+  title,
+  posted,
+  content,
+  views,
+  sponsored,
+  author {
+    name,
+    email
+  }
+}
+`
+  }
+});
---
+++ test/fixture/fragment/relay_createcontainer_hoc/source.js
@@ -0,0 +1,40 @@
+/* @flow */
+import React from "react";
+import Relay from "react-relay";
+import generateFragmentFromProps from "../../../../src/generateFragmentFromProps";
+
+type ArticleProps = {
+  article: {
+    title: string;
+    posted: string;
+    content: string;
+    views: number;
+    sponsored: boolean;
+
+    author: {
+      name: string;
+      email: string;
+    }
+  }
+};
+
+class Article extends React.Component<void, ArticleProps, void> {
+  render() {
+    const { article } = this.props;
+    return (
+      <div>
+        <div>{article.title} ({article.posted})</div>
+        <div>{article.author.name} [{article.author.email}]</div>
+        <div>{article.content}</div>
+      </div>
+    );
+  }
+}
+
+const RelayHOC = (component, options) => Relay.createContainer(component, options);
+
+export default RelayHOC(Article, {
+  fragments: {
+    article: generateFragmentFromProps()
+  }
+});
