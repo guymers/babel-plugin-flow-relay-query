0fa3dd7bbff6c5481f2e2b77ab1e9d19530dfab7
Kalley Powell <kalley@goco.io>
2017-03-31 16:02:27 -0500

    Adds AliasFor flowType that adds support for aliasing fields

--- src/utils/types.js
+++ src/utils/types.js
@@ -1,14 +1,14 @@
 /* @flow */
 /* eslint no-use-before-define:0 */
 
-type FlowBooleanType = { type: "boolean", nullable: boolean };
-type FlowNumberType = { type: "number", nullable: boolean };
-type FlowStringType = { type: "string", nullable: boolean };
-type FlowAnyType = { type: "any", nullable: boolean };
+type FlowBooleanType = { type: "boolean", nullable: boolean, fieldName?: string };
+type FlowNumberType = { type: "number", nullable: boolean, fieldName?: string };
+type FlowStringType = { type: "string", nullable: boolean, fieldName?: string };
+type FlowAnyType = { type: "any", nullable: boolean, fieldName?: string };
 
 export type FlowScalarType = FlowBooleanType | FlowNumberType | FlowStringType | FlowAnyType;
-export type FlowObjectType = { type: "object", nullable: boolean, properties: FlowTypes };
-export type FlowArrayType = { type: "array", child: FlowType };
+export type FlowObjectType = { type: "object", nullable: boolean, properties: FlowTypes, fieldName?: string };
+export type FlowArrayType = { type: "array", child: FlowType, fieldName?: string };
 export type FlowType = FlowObjectType | FlowScalarType | FlowArrayType;
 
 export type FlowTypes = { [name: string]: FlowType }
---
+++ test/fixture/fragment/field_aliases/expected.js
@@ -0,0 +1,52 @@
+/* @flow */
+import React from "react";
+import Relay from "react-relay";
+
+
+import type { AliasFor } from "../../../../src/flowTypes.js.flow";
+
+type ArticleProps = {
+  article: {
+    title: string;
+    posted: string;
+    content: string;
+    views: number;
+    sponsored: boolean;
+
+    writer: AliasFor<'author', {
+      fullName: AliasFor<'name', string>;
+      email: string;
+    }>;
+  }
+};
+
+class Article extends React.Component {
+  props: ArticleProps;
+
+  render() {
+    const { article } = this.props;
+    return <div>
+        <div>{article.title} ({article.posted})</div>
+        <div>{article.writer.fullName} [{article.writer.email}]</div>
+        <div>{article.content}</div>
+      </div>;
+  }
+}
+
+export default Relay.createContainer(Article, {
+  fragments: {
+    article: () => Relay.QL`
+fragment on Article {
+  title
+  posted
+  content
+  views
+  sponsored
+  writer: author {
+    fullName: name
+    email
+  }
+}
+`
+  }
+});
